27f19d34a277a19198356637eb50d8a5b15e3ffc
notapro192 <maxagin@gmail.com>
2014-11-16 08:59:57 -0500

    Project Euler number too long for int data type.  Need to rework for long data type. Test successful.

--- src/me/maxagin/Euler/LargestPrimeFactor.java
+++ src/me/maxagin/Euler/LargestPrimeFactor.java
@@ -1,9 +1,9 @@
 package me.maxagin.Euler;
 
 public class LargestPrimeFactor{
-    public static void main(String [] args){largePrimeFactor(600851475143)}
+    public static void main(String [] args){System.out.print(largePrimeFactor(13195));}
 
-    public static long largePrimeFactor (long num){
+    public static int largePrimeFactor (int num){
         for (int i = 2; i < num; i ++){
             while (divisible(i, num)){
                 num /= i;
@@ -12,10 +12,8 @@ public class LargestPrimeFactor{
         return num;
     }
 
-    public static boolean divisible(int i, long num){
-        double x = 0;
-        x = num / i;
-        x *= 10;
-        return ()
+    public static boolean divisible(int i, int num){
+        return (num % i == 0);
+
     }
 }
